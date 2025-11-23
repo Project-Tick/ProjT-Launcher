b02d8b0eb8644d6dfa22eee990544386d2cf115c
YongDo-Hyun <froster12@naver.com>
2025-11-23 22:26:49 +0300

    148. komit

--- tests/MojangVersionFormat_test.cpp
+++ tests/MojangVersionFormat_test.cpp
@@ -2,7 +2,6 @@
 #include <QTest>
 
 #include <minecraft/MojangVersionFormat.h>
-#include <ProblemProvider.h>
 
 class MojangVersionFormatTest : public QObject {
     Q_OBJECT
@@ -49,39 +48,6 @@ class MojangVersionFormatTest : public QObject {
         auto doc2 = MojangVersionFormat::versionFileToJson(vfile);
         writeJson("1.9-passthorugh.json", doc2);
         QCOMPARE(doc.toJson(), doc2.toJson());
-    }
-
-    void test_PlatformValidation()
-    {
-        ProblemContainer problems;
-
-        // Create a library with various platforms in natives
-        QJsonObject libObj;
-        libObj["name"] = "test:test:1.0";
-
-        QJsonObject nativesObj;
-        nativesObj["linux"] = "natives-linux.jar";                 // Should be accepted
-        nativesObj["windows"] = "natives-windows.jar";             // Should be accepted
-        nativesObj["osx"] = "natives-osx.jar";                     // Should be accepted
-        nativesObj["macos"] = "natives-macos.jar";                 // Should be accepted (alternative name)
-        nativesObj["freebsd"] = "natives-freebsd.jar";             // Should be accepted
-        nativesObj["unknown-platform"] = "natives-unknown.jar";    // Should be skipped
-        nativesObj["unsupported-os"] = "natives-unsupported.jar";  // Should be skipped
-
-        libObj["natives"] = nativesObj;
-
-        auto lib = MojangVersionFormat::libraryFromJson(problems, libObj, "test.json");
-
-        // Should have all supported platforms
-        QVERIFY(lib->m_nativeClassifiers.contains("linux"));
-        QVERIFY(lib->m_nativeClassifiers.contains("windows"));
-        QVERIFY(lib->m_nativeClassifiers.contains("osx"));
-        QVERIFY(lib->m_nativeClassifiers.contains("macos"));
-        QVERIFY(lib->m_nativeClassifiers.contains("freebsd"));
-
-        // Should NOT have unknown platforms
-        QVERIFY(!lib->m_nativeClassifiers.contains("unknown-platform"));
-        QVERIFY(!lib->m_nativeClassifiers.contains("unsupported-os"));
     }
 };
 
