87dee133d8d4beb10592711394f81e6cf84dac16
YongDo-Hyun <froster12@naver.com>
2026-01-06 16:45:37 +0300

    fix: Update deployment logic and folder structure in config and update scripts
    Signed-off-by: YongDo-Hyun <froster12@naver.com>

--- config.sh
+++ config.sh
@@ -4,14 +4,14 @@ export META_LAUNCHER_URL="${META_LAUNCHER_URL:-git@github.com:Project-Tick/meta-
 
 export META_UPSTREAM_DIR="${META_UPSTREAM_DIR:-upstream}"
 export META_LAUNCHER_DIR="${META_LAUNCHER_DIR:-metalauncher}"
-export DEPLOY_TO_FOLDER="${DEPLOY_TO_FOLDER:-false}"
-export DEPLOY_FOLDER="${DEPLOY_FOLDER:-/app/public/v1}"
+export DEPLOY_TO_FOLDER="${DEPLOY_TO_FOLDER:-true}"
+export DEPLOY_FOLDER="${DEPLOY_FOLDER:-./output}"
 export DEPLOY_FOLDER_USER="${DEPLOY_FOLDER_USER:-http}"
 export DEPLOY_FOLDER_GROUP="${DEPLOY_FOLDER_GROUP:-http}"
 
 export UPSTREAM_SSH="${UPSTREAM_SSH:-/home/samet/meta/config/key_upstream}"
 export LAUNCHER_SSH="${LAUNCHER_SSH:-/home/samet/meta/config/key_launcher}"
-export DEPLOY_TO_GIT="${DEPLOY_TO_GIT:-true}"
+export DEPLOY_TO_GIT="${DEPLOY_TO_GIT:-false}"
 export GIT_AUTHOR_NAME="${GIT_AUTHOR_NAME:-Project Tick Bot}"
 export GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL:-github@bot.yongdohyun.org.tr}"
 export GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME:-$GIT_AUTHOR_NAME}"
