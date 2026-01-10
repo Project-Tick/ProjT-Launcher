b5c7fc1da1352d096a112ab99ed0100ffa01b306
YongDo-Hyun <froster12@naver.com>
2026-01-10 18:42:44 +0300

    fix: Ensure proper header guards and include zutil.h in inftrees.h
    Signed-off-by: YongDo-Hyun <froster12@naver.com>

--- zlib/inftrees.h
+++ zlib/inftrees.h
@@ -8,6 +8,11 @@
    subject to change. Applications should only use zlib.h.
  */
 
+#ifndef INFTREES_H
+#define INFTREES_H
+
+#include "zutil.h"
+
 /* Structure for decoding tables.  Each entry provides either the
    information needed to do the operation requested by the code that
    indexed that table entry, or it provides a pointer to another
@@ -55,3 +60,5 @@ typedef enum { CODES, LENS, DISTS } codetype;
 
 int ZLIB_INTERNAL
 inflate_table(codetype type, unsigned short FAR* lens, unsigned codes, code FAR* FAR* table, unsigned FAR* bits, unsigned short FAR* work);
+
+#endif /* INFTREES_H */
