b85197ea6e8f2afafcd414555af0f0e966daee57
YongDo-Hyun <froster12@naver.com>
2026-01-11 23:23:30 +0300

    refactor: improve code formatting and consistency in getopt implementation

--- zlib/zconf.h
+++ zlib/zconf.h
@@ -343,10 +343,10 @@ typedef unsigned long z_size_t;
 #endif
 #endif
 #endif /* ZLIB_DLL */
-	   /* If building or using zlib with the WINAPI/WINAPIV calling convention,
-		* define ZLIB_WINAPI.
-		* Caution: the standard ZLIB1.DLL is NOT compiled using ZLIB_WINAPI.
-		*/
+/* If building or using zlib with the WINAPI/WINAPIV calling convention,
+ * define ZLIB_WINAPI.
+ * Caution: the standard ZLIB1.DLL is NOT compiled using ZLIB_WINAPI.
+ */
 #ifdef ZLIB_WINAPI
 #ifdef FAR
 #undef FAR
