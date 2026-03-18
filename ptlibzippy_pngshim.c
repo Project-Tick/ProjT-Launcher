3b0f276604bc5cfe06727a83732a7e90c33da110
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-03-18 09:14:18 +0000

    add PTLIBZIPPY_INTERNAL preprocessor to pngshim.c
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- ptlibzippy_pngshim.c
+++ ptlibzippy_pngshim.c
@@ -3,6 +3,7 @@
  * For conditions of distribution and use, see copyright notice in ptlibzippy.h
  */
 
+#define PTLIBZIPPY_INTERNAL
 #include "ptlibzippy.h"
 
 uLong ZEXPORT ptpng_adler32(uLong adler, const Bytef *buf, uInt len) {
